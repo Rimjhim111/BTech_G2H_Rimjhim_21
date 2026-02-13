5f71bdd5608284b023db452eb0cf93ce07bbe151
Rimjhim111 <25110cn564@glbitm.ac.in>
2026-02-13 11:05:39 +0530

    Add files via upload

---
+++ equalNumber.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include <math.h>
+int main()
+{
+    float a ,b;
+    scanf("%f%f",&a,&b);
+    if (a = b){
+        printf("Numbers are equal");
+    } else{
+        printf("nmbers are not equal");
+    }
+}
